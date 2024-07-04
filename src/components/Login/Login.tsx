478c291d741d6d6216c0fd1e598f8e15243ca546
Charlie-Gauvin <gauvinch9@gmail.com>
2024-07-04 11:25:56 +0200

    feat: :bug: Bug CORS error with fetch - in progress

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,12 +1,93 @@
 import { Link } from 'react-router-dom';
 import './Login.scss';
+import { useEffect, useState } from 'react';
+
 
 function Login() {
+
+  // Déclaration des states
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // Fonction pour gérer le submit du formulaire
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const response = await fetchdata();
+
+    if (!response) {
+      console.log('Erreur de connexion');
+    } else {
+      console.log('Connexion réussie');
+      console.log(response);
+    }
+  }
+
+
+
+  // Fonction pour récupérer les données de l'API
+  const fetchdata = async () => {
+
+    try {
+      const response = await fetch(`http://localhost:3000/api/v1/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          // Ajoutez d'autres en-têtes ici si nécessaire, comme un en-tête d'autorisation
+          credentials: 'include'
+        },
+        mode: 'cors',
+        body: JSON.stringify({email, password})
+      });
+      const data = await response.json();
+      console.log(data);
+      return data;
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchdata();
+  }, [])
+
+
+  // Fonction pour récupérer les données de l'API
+  const fetchget = async () => { 
+    try {
+      const response = await fetch(`http://localhost:3000/api/v1/games`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          // Ajoutez d'autres en-têtes ici si nécessaire, comme un en-tête d'autorisation
+          credentials: 'include'
+        },
+        mode: 'cors',
+      });
+      const data = await response.json();
+      console.log(data);
+      return data;
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchget();
+  }, [])
+
+
+
+
+
+
   return (
     <div className="login">
       <section className="login__connexion">
         <h2 className="login__title">Connexion</h2>
-        <form className="login__form">
+        <form className="login__form" onSubmit={handleSubmit}>
           <div className="login__form-group">
             <label className="login__form-label" htmlFor="email">
               Email
@@ -17,6 +98,8 @@ function Login() {
               placeholder="email"
               name="email"
               id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="login__form-group">
@@ -28,12 +111,14 @@ function Login() {
               type="password"
               placeholder="password"
               name="password"
+              value={password}
+              onChange = {(e) => setPassword(e.target.value)}
             />
+            <button type="submit" className="login__button__style">
+              Connexion
+            </button>
           </div>
         </form>
-        <button type="submit" className="login__button">
-          Connexion
-        </button>
         <p className="login__forgot-password">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
           consectetur incidunt -
