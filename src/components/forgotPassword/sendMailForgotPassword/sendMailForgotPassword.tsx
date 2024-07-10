c7704dbbd30b647173aaac6b8daf3c490e0cec93
alexandrehamedOC <alexandre.hamed@oclock.school>
2024-07-10 17:57:19 +0200

    mail ok, must add front notif

--- src/components/forgotPassword/sendMailForgotPassword/sendMailForgotPassword.tsx
+++ src/components/forgotPassword/sendMailForgotPassword/sendMailForgotPassword.tsx
@@ -1,6 +1,30 @@
+import { useState } from 'react';
 import './sendMailForgotPassword.scss';
+import axios from 'axios';
 
 export default function SendMailForgotPassword({}) {
+  const [email, setEmail] = useState('renseigner votre mail');
+  const [notification,setNotication] = useState('');
+
+  function handleGetMail(inputValue: string) {
+    setEmail(inputValue);
+  }
+  function handleSubmitForm(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    axios
+      .post('http://localhost:3000/api/v1/forgot-password', {
+        email: email,
+      })
+      .then(function (response) {
+        setNotication('Email envoyé avec succès !');
+        console.log(response);
+      })
+      .catch(function (error) {
+        setNotication('Erreur lors de l\'envoi de l\'email. Veuillez réessayer.');
+        console.log(error);
+      });
+  }
   return (
     <div className="main__container">
       <h1 className="main__title">Mot de passe oublié</h1>
@@ -8,7 +32,7 @@ export default function SendMailForgotPassword({}) {
         Un email va vous être envoyé si vous êtes bien enregistrer dans notre
         base de donnée.
       </p>
-      <form className="login__form">
+      <form className="login__form" onSubmit={handleSubmitForm}>
         <div className="login__form-group">
           <label className="login__form-label" htmlFor="email">
             Email
@@ -19,11 +43,12 @@ export default function SendMailForgotPassword({}) {
             placeholder="email"
             name="email"
             id="email"
+            onChange={(event) => handleGetMail(event.target.value)}
           />
-          
         </div>
         <button>ENVOYER</button>
       </form>
+      {notification && <div className="notification">{notification}</div>}
     </div>
   );
 }
