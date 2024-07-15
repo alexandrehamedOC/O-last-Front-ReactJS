26b024bb5534e8d214e994058150dab3c87019c3
JulienMedina <julien.medina16@gmail.com>
2024-07-15 21:30:12 +0200

    feat: add test for App.tsx in App.test.tsx

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -26,7 +26,7 @@
 //     expect(screen.getByText("Bienvenue sur O'Last !")).toBeInTheDocument();
 //   });
 // });
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
 import App from '../src/components/App/App';
 import '@testing-library/jest-dom/extend-expect';
@@ -35,5 +35,25 @@ describe('render App', () => {
   it('should render App', () => {
     render(<App />);
     expect(screen.getByText("Bienvenue sur O'Last !")).toBeInTheDocument();
+  });
+
+  it('should render input', () => {
+    render(<App />);
+    expect(
+      screen.getByPlaceholderText('Chercher un jeu ...')
+    ).toBeInTheDocument();
+  });
+
+  it('should render the start button', () => {
+    render(<App />);
+    const startButton = screen.getByRole('button', { name: 'Start !' });
+    expect(startButton).toBeInTheDocument();
+  });
+
+  it('should handle button click', () => {
+    render(<App />);
+    const startButton = screen.getByRole('button', { name: 'Start !' });
+    fireEvent.click(startButton);
+    expect(startButton).toBeInTheDocument();
   });
 });
