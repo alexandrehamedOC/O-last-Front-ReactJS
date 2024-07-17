e5644d5e27bfecf001f79fd6b96650bc41b08994
alexandrehamedOC <alexandre.hamed@oclock.school>
2024-07-17 15:12:01 +0200

    css edit finish

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,7 @@ import logoLol from '../../assets/img/games-logo/Lol-logo.png';
 import logolMinecraft from '../../assets/img/games-logo/minecraft.png';
 import leftarrow from '../../assets/img/fleche-gauche.png';
 import rightarrow from '../../assets/img/fleche-droite.png';
+import manette from '../../assets/img/console-de-jeu.png';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { Navigate, useNavigate } from 'react-router-dom';
@@ -137,7 +138,20 @@ function App() {
             </div>
           </section>
         </section>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#1a1a1a" fill-opacity="1" d="M0,224L48,229.3C96,235,192,245,288,250.7C384,256,480,256,576,250.7C672,245,768,235,864,192C960,149,1056,75,1152,80C1248,85,1344,171,1392,213.3L1440,256L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+        {/*Old rose <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#e998e6"
+            fill-opacity="1"
+            d="M0,320L48,309.3C96,299,192,277,288,250.7C384,224,480,192,576,192C672,192,768,224,864,224C960,224,1056,192,1152,170.7C1248,149,1344,139,1392,133.3L1440,128L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+          ></path>
+        </svg> */}
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#1a1a1a"
+            fill-opacity="1"
+            d="M0,224L48,229.3C96,235,192,245,288,250.7C384,256,480,256,576,250.7C672,245,768,235,864,192C960,149,1056,75,1152,80C1248,85,1344,171,1392,213.3L1440,256L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+          ></path>
+        </svg>
 
         {/*Old disoplay*/}
         {/* <section className="app__step">
@@ -222,7 +236,48 @@ function App() {
             </section>
           </section>
         </section>
-
+        <svg
+          id="sw-js-blob-svg"
+          viewBox="0 0 100 100"
+          xmlns="http://www.w3.org/2000/svg"
+          className="background-svg"
+        >
+          <defs>
+            <linearGradient id="sw-gradient" x1="0" x2="1" y1="1" y2="0">
+              <stop
+                id="stop1"
+                stopColor="rgba(147, 26, 206, 1)"
+                offset="0%"
+              ></stop>
+              <stop
+                id="stop2"
+                stopColor="rgba(26, 26, 26, 1)"
+                offset="100%"
+              ></stop>
+            </linearGradient>
+          </defs>
+          <path
+            fill="url(#sw-gradient)"
+            d="M16,-25.1C22.2,-24.2,29.6,-22.8,32.8,-18.5C36.1,-14.3,35.1,-7.1,35.6,0.3C36.2,7.7,38.2,15.5,34.8,19.6C31.5,23.6,22.8,24,16.1,28.1C9.4,32.2,4.7,40,-0.1,40.3C-5,40.5,-10,33.3,-16.9,29.3C-23.8,25.3,-32.6,24.6,-35.8,20.3C-38.9,15.9,-36.4,8,-33.1,1.9C-29.9,-4.2,-25.9,-8.4,-23.2,-13.5C-20.5,-18.7,-19.1,-24.7,-15.5,-27C-11.8,-29.3,-5.9,-28,-0.5,-27.1C4.9,-26.3,9.9,-25.9,16,-25.1Z"
+            width="100%"
+            height="100%"
+            transform="translate(50 50)"
+            strokeWidth="0"
+            style={{ transition: 'all 0.3s ease 0s' }}
+          ></path>
+        </svg>
+        <div className="vertical-dots">
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+        </div>
+        <section className="app__manette">
+          <section className="app__manette-container">
+            <figure className="app__manette-image">
+              <img src={manette} alt="" />
+            </figure>
+          </section>
+        </section>
         <section className="app__slider-container">
           <h2 className="app__title-reviews">Retour utilisateur</h2>
           <section className="slider">
@@ -240,25 +295,24 @@ function App() {
                 }}
               >
                 <li className="slider__slide">
-                  <h3>LucasW</h3>
+                  <h3>VirginieF0F</h3>
                   <p>
-                    « O'Last a vraiment changé ma façon de jouer ! J'ai trouvé
-                    des coéquipiers pour Minecraft en un rien de temps. La
-                    plateforme est super intuitive et facile à utiliser. Merci
-                    pour ce service incroyable ! »
+                    « Je suis déçue de ne pas trouver Mario Kart sur O'Last,
+                    c'est mon jeu préféré. Cependant, la plateforme est bien
+                    conçue et la communauté est sympa. J'espère que Mario Kart
+                    sera ajouté bientôt ! »
                   </p>
                 </li>
                 <li className="slider__slide">
-                  <h3>Clara_92</h3>
+                  <h3>Lorenzo</h3>
                   <p>
-                    « J'adore League of Legends, et grâce à O'Last, j'ai pu
-                    former une équipe géniale pour mes parties ranked. Les
-                    options de filtrage sont super utiles pour trouver des
-                    joueurs avec les mêmes objectifs. Recommandé à 100% ! »
+                    « Avant je n'avais pas d'amis et j'étais seul, pour jouer.
+                    Grâce vous j'ai pu rencontrer Yann G, et nous faisons des
+                    parties endiablé ensemble jusqu'au bout de la nuit. »
                   </p>
                 </li>
                 <li className="slider__slide">
-                  <h3>MaximeGaming</h3>
+                  <h3>VM O'Clock</h3>
                   <p>
                     « O'Last est une plateforme fantastique pour les joueurs de
                     Fortnite ! Je peux facilement trouver des squads pour mes
@@ -324,7 +378,14 @@ function App() {
             <img src={rightarrow} alt="rightarrow" />
           </button>
         </section> */}
-        <section className="app__annonce-container">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#6164f8"
+            fill-opacity="1"
+            d="M0,320L48,309.3C96,299,192,277,288,250.7C384,224,480,192,576,192C672,192,768,224,864,224C960,224,1056,192,1152,170.7C1248,149,1344,139,1392,133.3L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+          ></path>
+        </svg>
+        {/* <section className="app__annonce-container">
           <section className="app__annonce">
             <p className="app__annonce-description">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora
@@ -334,7 +395,7 @@ function App() {
             </p>
             <button className="app__annonce-button">PLAY</button>
           </section>
-        </section>
+        </section> */}
       </section>
     </div>
   );
