56e7e87d7bc5e76e2cddf0b189a5362eb3f16f5e
alexandrehamedOC <alexandre.hamed@oclock.school>
2024-07-17 11:37:16 +0200

    end rework

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import './App.scss';
 import logoValorant from '../../assets/img/games-logo/Valorant-logo.png';
 import logoLol from '../../assets/img/games-logo/Lol-logo.png';
-import logolMinecraft from '../../assets/img/games-logo/minecraft.png'
+import logolMinecraft from '../../assets/img/games-logo/minecraft.png';
 import leftarrow from '../../assets/img/fleche-gauche.png';
 import rightarrow from '../../assets/img/fleche-droite.png';
 import axios from 'axios';
@@ -34,6 +34,17 @@ function App() {
   ) => {
     navigate(`/Annonce?game_id=${selectedGame}`);
   };
+
+  //Sliders horizontal
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const totalSlides = 4;
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % totalSlides);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + totalSlides) % totalSlides);
+  };
   return (
     <div className="app">
       <section className="app__searchbar">
@@ -43,97 +54,92 @@ function App() {
           type="text"
           placeholder="Chercher un jeu ..."
         /> */}
-        <section className='searchArea'>
-        <select
-          name="searchBar"
-          id="searchBar"
-          value={selectedGame}
-          onChange={handleSelectChange}
-        >
-          <option value="" disabled>
-            Choisir un jeu
-          </option>
-          {games.map((item: any) => (
-            <option value={item.id} key={item.id}>
-              {item.name}
+        <section className="searchArea">
+          <select
+            name="searchBar"
+            id="searchBar"
+            value={selectedGame}
+            onChange={handleSelectChange}
+          >
+            <option value="" disabled>
+              Choisir un jeu
             </option>
-          ))}
-        </select>
-        <button
-          className="app__button"
-          onClick={(e) => handleGamesClick(e, selectedGame)}
-        >
-          Start !
-        </button>
+            {games.map((item: any) => (
+              <option value={item.id} key={item.id}>
+                {item.name}
+              </option>
+            ))}
+          </select>
+          <button
+            className="app__button"
+            onClick={(e) => handleGamesClick(e, selectedGame)}
+          >
+            Start !
+          </button>
         </section>
-
       </section>
       <section className="app__pagesizer">
+        <section className="app__games">
+          <section className="element__wrapper">
+            <div className="app__games-images-container">
+              <figure className="app__games-figure image__Home__1">
+                <img
+                  className="app_games-image"
+                  src={logoLol}
+                  alt="logo Valorant"
+                />
+              </figure>
+              <figure className="app__games-figure image__Home__2">
+                <img
+                  className="app_games-image"
+                  src={logoValorant}
+                  alt="logo League of Legends"
+                />
+              </figure>
+              <figure className="app__games-figure image__Home__3">
+                <img
+                  className="app_games-image"
+                  src={logolMinecraft}
+                  alt="logo Minecraft"
+                />
+              </figure>
+            </div>
 
-      
-      <section className="app__games">
-        <section className='element__wrapper'>
-        <div className="app__games-images-container">
-          <figure className="app__games-figure image__Home__1">
-            <img
-              className="app_games-image"
-              src={logoLol}
-              alt="logo Valorant"
-            />
-          </figure>
-          <figure className="app__games-figure image__Home__2">
-            <img
-              className="app_games-image"
-              src={logoValorant}
-              alt="logo League of Legends"
-            />
-          </figure>
-          <figure className="app__games-figure image__Home__3">
-            <img
-              className="app_games-image"
-              src={logolMinecraft}
-              alt="logo Minecraft"
-            />
-          </figure>
-        </div>
-
-        <div className='app__responsive-logo-area'>
-        <figure className="">
-            <img
-              className=""
-              src={logoLol}
-              alt="logo Valorant"
-            />
-          </figure>
-          <figure className="">
-            <img
-              className=""
-              src={logoValorant}
-              alt="logo League of Legends"
-            />
-          </figure>
-          <figure className="">
-            <img
-              className=""
-              src={logolMinecraft}
-              alt="logo Minecraft"
-            />
-          </figure>
-        </div>
-        <div className="app__games-container">
-          <h2 className="app__games-title">Jeux disponibles</h2>
-          <p className="app__games-description">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio
-            itaque facere suscipit reiciendis nostrum rerum fuga quam nam beatae
-            quia quibusdam adipisci quae possimus asperiores doloremque
-            delectus, veritatis excepturi alias. Modi unde molestias veniam
-            soluta quo aspernatur ut praesentium atque exercitationem distinctio
-            asperiores reiciendis harum pariatur.
-          </p>
-        </div>
+            <div className="app__responsive-logo-area">
+              <figure className="">
+                <img className="" src={logoLol} alt="logo Valorant" />
+              </figure>
+              <figure className="">
+                <img
+                  className=""
+                  src={logoValorant}
+                  alt="logo League of Legends"
+                />
+              </figure>
+              <figure className="">
+                <img className="" src={logolMinecraft} alt="logo Minecraft" />
+              </figure>
+            </div>
+            <div className="app__games-container">
+              <h2 className="app__games-title">Jeux disponibles</h2>
+              <p className="app__games-description">
+                une plateforme innovante dédiée aux passionnés de jeux vidéo,
+                vous permettant de poster des annonces pour trouver d'autres
+                joueurs et jouer ensemble. Que vous soyez amateur de Minecraft,
+                League of Legends, Fortnite ou Call of Duty, notre site vous
+                aide à former des équipes ou à rejoindre des groupes existants.
+                Avec une interface intuitive et des fonctionnalités avancées,
+                O'last facilite les rencontres entre gamers partageant les mêmes
+                intérêts. Rejoignez notre communauté et vivez des expériences de
+                jeu uniques et mémorables. Connectez-vous, publiez votre annonce
+                et commencez à jouer dès aujourd'hui !
+              </p>
+            </div>
+          </section>
         </section>
-      </section>
-      <section className="app__step">
+
+        {/*Old disoplay*/}
+        {/* <section className="app__step">
         <h3 className="app__step-title">Etapes pour utiliser O'Last</h3>
         <ol className="app__step-list">
           <li className="app__step-item">
@@ -169,45 +175,165 @@ function App() {
             </p>
           </li>
         </ol>
-      </section>
-      <section className="app__reviews">
-        <button className="app__reviews-leftarrow">
-          <img src={leftarrow} alt="leftarrow" />
-        </button>
-        <div className="app__reviews-card">
-          <h3 className="app__reviews-username">Pseudo</h3>
-          <p className="app_reviews-text">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius
-            tempore inventore omnis! Quisquam, quos. Quisquam, quos.
-          </p>
-        </div>
-        <div className="app__reviews-card">
-          <h3 className="app__reviews-username">Pseudo</h3>
-          <p className="app_reviews-text">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius
-            tempore inventore omnis! Quisquam, quos. Quisquam, quos.
-          </p>
-        </div>
-        <div className="app__reviews-card">
-          <h3 className="app__reviews-username">Pseudo</h3>
-          <p className="app_reviews-text">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius
-            tempore inventore omnis! Quisquam, quos. Quisquam, quos.
-          </p>
-        </div>
-        <button className="app__reviews-rightarrow">
-          <img src={rightarrow} alt="rightarrow" />
-        </button>
-      </section>
-      <section className="app__annonce">
-        <p className="app__annonce-description">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora
-          suscipit, magni nisi eligendi quod incidunt, accusantium obcaecati
-          dolore non aliquam magnam enim repudiandae? Ducimus temporibus nostrum
-          porro quidem cum at!
-        </p>
-        <button className="app__annonce-button">PLAY</button>
-      </section>
+      </section> */}
+
+        <section className="app__use">
+          <section className="app__use-title">
+            <h1>Etapes pour utiliser O'Last</h1>
+          </section>
+          <section className="app__use__container">
+            <section className="app__use__card-container">
+              <section className="app__use-card">
+                <div className="app__use-card-number">1</div>
+                <h3>Rechercher un jeu</h3>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Excepturi sed a.
+                </p>
+              </section>
+
+              <section className="app__use-card">
+                <div className="app__use-card-number">2</div>
+                <h3>Sélectionner un jeu</h3>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Excepturi sed a.
+                </p>
+              </section>
+
+              <section className="app__use-card">
+                <div className="app__use-card-number">3</div>
+                <h3>Trouver un joueurs</h3>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Excepturi sed a.
+                </p>
+              </section>
+
+              <section className="app__use-card">
+                <div className="app__use-card-number">4</div>
+                <h3>Jouer !</h3>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Excepturi sed a.
+                </p>
+              </section>
+            </section>
+          </section>
+        </section>
+
+        <section className="app__slider-container">
+          <h2 className="app__title-reviews">Retour utilisateur</h2>
+          <section className="slider">
+            <button
+              className="slider__button slider__button--prev"
+              onClick={prevSlide}
+            >
+              ‹
+            </button>
+            <div className="slider__track-container">
+              <ul
+                className="slider__track"
+                style={{
+                  transform: `translateX(-${(currentIndex * 100) / 3}%)`,
+                }}
+              >
+                <li className="slider__slide">
+                  <h3>LucasW</h3>
+                  <p>
+                    « O'Last a vraiment changé ma façon de jouer ! J'ai trouvé
+                    des coéquipiers pour Minecraft en un rien de temps. La
+                    plateforme est super intuitive et facile à utiliser. Merci
+                    pour ce service incroyable ! »
+                  </p>
+                </li>
+                <li className="slider__slide">
+                  <h3>Clara_92</h3>
+                  <p>
+                    « J'adore League of Legends, et grâce à O'Last, j'ai pu
+                    former une équipe géniale pour mes parties ranked. Les
+                    options de filtrage sont super utiles pour trouver des
+                    joueurs avec les mêmes objectifs. Recommandé à 100% ! »
+                  </p>
+                </li>
+                <li className="slider__slide">
+                  <h3>MaximeGaming</h3>
+                  <p>
+                    « O'Last est une plateforme fantastique pour les joueurs de
+                    Fortnite ! Je peux facilement trouver des squads pour mes
+                    sessions de jeu. L'interface est claire et le processus de
+                    création d'annonce est rapide. »
+                  </p>
+                </li>
+                <li className="slider__slide">
+                  <h3>SarahJ</h3>
+                  <p>
+                    « Je suis une grande fan de Call of Duty et je cherchais un
+                    groupe pour les parties en ligne. O'Last a parfaitement
+                    répondu à mes attentes. J'ai trouvé une équipe solide et
+                    nous avons passé des soirées de jeu fantastiques ! »
+                  </p>
+                </li>
+                <li className="slider__slide">
+                  <h3>GamingNico</h3>
+                  <p>
+                    « Super plateforme pour les gamers ! J'ai utilisé O'Last
+                    pour trouver des partenaires pour Minecraft, et le processus
+                    a été très simple. Les options de communication sont
+                    excellentes et j'ai rencontré des gens vraiment sympas. »
+                  </p>
+                </li>
+              </ul>
+            </div>
+            <button
+              className="slider__button slider__button--next"
+              onClick={nextSlide}
+            >
+              ›
+            </button>
+          </section>
+        </section>
+
+        {/*OLD SLIDDER <section className="app__reviews">
+          <button className="app__reviews-leftarrow">
+            <img src={leftarrow} alt="leftarrow" />
+          </button>
+          <div className="app__reviews-card">
+            <h3 className="app__reviews-username">Pseudo</h3>
+            <p className="app_reviews-text">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius
+              tempore inventore omnis! Quisquam, quos. Quisquam, quos.
+            </p>
+          </div>
+          <div className="app__reviews-card">
+            <h3 className="app__reviews-username">Pseudo</h3>
+            <p className="app_reviews-text">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius
+              tempore inventore omnis! Quisquam, quos. Quisquam, quos.
+            </p>
+          </div>
+          <div className="app__reviews-card">
+            <h3 className="app__reviews-username">Pseudo</h3>
+            <p className="app_reviews-text">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius
+              tempore inventore omnis! Quisquam, quos. Quisquam, quos.
+            </p>
+          </div>
+          <button className="app__reviews-rightarrow">
+            <img src={rightarrow} alt="rightarrow" />
+          </button>
+        </section> */}
+        <section className="app__annonce-container">
+          <section className="app__annonce">
+            <p className="app__annonce-description">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora
+              suscipit, magni nisi eligendi quod incidunt, accusantium obcaecati
+              dolore non aliquam magnam enim repudiandae? Ducimus temporibus
+              nostrum porro quidem cum at!
+            </p>
+            <button className="app__annonce-button">PLAY</button>
+          </section>
+        </section>
       </section>
     </div>
   );
